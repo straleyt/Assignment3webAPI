a77100313303b49b744b6a268c51e6720d8a2d21
straleyt <tegan.straley@ucdenver.edu>
2018-03-04 09:39:44 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+// singup POST
+// singin POST
+//get all GET
+// save new POST
+//get specific GET
+//save exisiting PUT
+//delete specific DELETE
+//in class
+
+
+//sin up, signing, posts, all errors messages
+
+
+//but with movies! singup signin users
+//then movies update, etc. 
+
+//authJWTController. isAuthenticated
+
+//mongoose.connect process.env.DB);
+//DB = mongod://localhost/webapi2
+//#DB = mongodb://node:mlab@me@passworsword.mlab.com:892898/webapi
