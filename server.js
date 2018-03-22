d58364f0654e007b8d853c2c2841343edc8a1940
straleyt <tegan.straley@ucdenver.edu>
2018-03-22 13:47:15 -0600

    fix: all assignment 4 requirements met

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(passport.initialize());
 var router = express.Router(); // get instance of router
 
-mongoose.connect(process.env.DB , (err, database) => {
+mongoose.connect(process.env.DB, (err, database) => {
     if (err) throw err;
     console.log("Connected to the database.");
     db = database;
@@ -37,20 +37,20 @@ mongoose.connect(process.env.DB , (err, database) => {
 //~~~~~~USER schema~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 var userSchema = new Schema({
     name: String,
-    username: {type: String, required: true, index: { unique: true}}, 
-    password: {type: String, required: true, select: false}
+    username: { type: String, required: true, index: { unique: true } },
+    password: { type: String, required: true, select: false }
 });
 var User = mongoose.model("User", userSchema);
 
 // hash the password before the user is saved
-userSchema.pre('save', function(next) {
+userSchema.pre('save', function (next) {
     var user = this;
 
     // hash the password only if the password has been changed or user is new
     if (!user.isModified('password')) return next();
 
     // generate the hash
-    bcrypt.hash(user.password, null, null, function(err, hash) {
+    bcrypt.hash(user.password, null, null, function (err, hash) {
         if (err) return next(err);
 
         // change the password to the hashed version
@@ -60,40 +60,45 @@ userSchema.pre('save', function(next) {
 });
 
 //~~~~~~MOVIE schema~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-function minLength(actors){
+function minLength(actors) {
     return actors.length >= 3;
 }
 
 var movieSchema = new Schema({
-    title: {type: String, required: true, unique: true},
-    yearReleased: {type: Number, min:1600, max:9999}, 
-    genre: {type: String, required: true, enum: ['Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Thriller', 'Western']},
-    actors: {type:[
-        {name:{type: String, required: true},
-        characterName: {type: String, required: true}}]
-        ,validate:[minLength, "Must be a minimum of 3 actors"]}
+    title: { type: String, required: true, unique: true },
+    yearReleased: { type: Number, min: 1600, max: 9999 },
+    genre: { type: String, required: true, enum: ['Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Thriller', 'Western'] },
+    actors: {
+        type: [
+            {
+                name: { type: String, required: true },
+                characterName: { type: String, required: true }
+            }]
+        , validate: [minLength, "Must be a minimum of 3 actors"]
+    }
 });
 var Movie = mongoose.model("Movie", movieSchema);
 
 //~~~~~~REVIEW schema~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 var reviewSchema = new Schema({
-    reviewer: {type: String, required:true},
-    quote: {type: String, required:true},
-    rating: {type: Number, min:1, max:5, required:true}
+    reviewer: { type: String, required: true },
+    quote: { type: String, required: true },
+    rating: { type: Number, min: 1, max: 5, required: true },
+    movietitle: { type: String, required: true }
 });
 var Review = mongoose.model("Review", reviewSchema);
 
 //~~~~~~signup and signin~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 router.post('/signup', (req, res) => {
     if (!req.body.username || !req.body.password || !req.body.name) {
-        res.status(422).json({success: false, msg: 'Please pass username, name, and password.'});
+        res.status(422).json({ success: false, msg: 'Please pass username, name, and password.' });
     } else {
         var newUser = new User(req.body);
-        
+
         // save the user
         newUser.save()
             .then(item => {
-                res.json({success: true, msg: 'Successful created new user.'});
+                res.json({ success: true, msg: 'Successful created new user.' });
             })
             .catch(err => {
                 res.status(422).send("Unable to save to database");
@@ -105,37 +110,37 @@ router.post('/signup', (req, res) => {
 router.post('/signin', (req, res) => {
     var userNew = new User(req.body);
 
-    User.findOne({ username: userNew.username }).select('name username password').exec(function(err, user) {
+    User.findOne({ username: userNew.username }).select('name username password').exec(function (err, user) {
         if (err) res.send(err);
 
         //bcrypt compares unhashed password and saved hashed password 
         bcrypt.compare(userNew.password, user.password, (err, isMatch) => {
             if (isMatch) {
-                var userToken = {id: user._id, username: user.username};
+                var userToken = { id: user._id, username: user.username };
                 var token = jwt.sign(userToken, process.env.SECRET_KEY);
-                res.json({success: true, message: 'Enjoy the jwt token!', token: token});
+                res.json({ success: true, message: 'Enjoy the jwt token!', token: token });
             }
             else {
-                res.status(401).send({success: false, msg: 'Authentication failed. Wrong password.'});
+                res.status(401).send({ success: false, msg: 'Authentication failed. Wrong password.' });
             }
         });
     });
 });
 
 //~~~~~~Middle-route: Authentication~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-router.use('/movies',(req, res, next) => { 
+router.use('/movies', (req, res, next) => {
     //First must authenticate
-    var token = req.headers['x-access-token'] || req.body.token || req.query.token; 
+    var token = req.headers['x-access-token'] || req.body.token || req.query.token;
     var secretOrKey = process.env.SECRET_KEY;
     //console.log("Token:  " + token);
-    if (token != null) { 
-        jwt.verify(token, secretOrKey, function(err, decoded) { 
-            if (err) { 
-                return res.status(403).send({ 
-                    success: false, 
-                    message: 'Failed to authenticate token.' 
+    if (token != null) {
+        jwt.verify(token, secretOrKey, function (err, decoded) {
+            if (err) {
+                return res.status(403).send({
+                    success: false,
+                    message: 'Failed to authenticate token.'
                 });
-            } else { 
+            } else {
                 console.log("User authenticated.");
                 req.decoded = decoded;
                 next();
@@ -143,25 +148,25 @@ router.use('/movies',(req, res, next) => {
         });
     } else {
         return res.status(403).send({
-        success: false,
-        message: 'No token provided.'
+            success: false,
+            message: 'No token provided.'
         });
     }
 });
 
-router.use('/reviews',(req, res, next) => { 
+router.use('/reviews', (req, res, next) => {
     //First must authenticate
-    var token = req.headers['x-access-token'] || req.body.token || req.query.token; 
+    var token = req.headers['x-access-token'] || req.body.token || req.query.token;
     var secretOrKey = process.env.SECRET_KEY;
     //console.log("Token:  " + token);
-    if (token != null) { 
-        jwt.verify(token, secretOrKey, function(err, decoded) { 
-            if (err) { 
-                return res.status(403).send({ 
-                    success: false, 
-                    message: 'Failed to authenticate token.' 
+    if (token != null) {
+        jwt.verify(token, secretOrKey, function (err, decoded) {
+            if (err) {
+                return res.status(403).send({
+                    success: false,
+                    message: 'Failed to authenticate token.'
                 });
-            } else { 
+            } else {
                 console.log("User authenticated.");
                 req.decoded = decoded;
                 next();
@@ -169,8 +174,8 @@ router.use('/reviews',(req, res, next) => {
         });
     } else {
         return res.status(403).send({
-        success: false,
-        message: 'No token provided.'
+            success: false,
+            message: 'No token provided.'
         });
     }
 });
@@ -183,19 +188,19 @@ router.post('/movies/save', (req, res) => {
     console.log("Going to save a movie...");
 
     //Needs title, yearReleased, genre, >= 3 actorName, >= 3 actorCharacterName
-    if (    !req.body.title
-         || !req.body.yearReleased
-         || !req.body.genre 
-         || !req.body.actors
-        ) {
-        res.json({success: false, msg: 'Please pass title, yearReleased, genre, actors.'});
-    } 
-    else { 
+    if (!req.body.title
+        || !req.body.yearReleased
+        || !req.body.genre
+        || !req.body.actors
+    ) {
+        res.json({ success: false, msg: 'Please pass title, yearReleased, genre, actors.' });
+    }
+    else {
         var newMovie = new Movie(req.body)
         // save the user
         newMovie.save()
             .then(item => {
-                res.json({success: true, msg: 'Successful created new movie.'});
+                res.json({ success: true, msg: 'Successful created new movie.' });
             })
             .catch(err => {
                 res.status(422).send("Unable to save movie to database");
@@ -208,64 +213,80 @@ router.post('/movies/save', (req, res) => {
 router.get('/movies/get', (req, res) => {
     console.log("Going to get a movie...");
     var id = req.headers._id;
-    Movie.findById(id, function(err, movie) {
-      if (err) res.send(err);
-  
-      var userJson = JSON.stringify(movie);
-      // return that user
-      res.json(movie);
+    var reviewOption = req.headers.review;
+    Movie.findById(id, function (err, movie) {
+        if (err) res.send(err);
+        if (reviewOption == "false"){
+            var userJson = JSON.stringify(movie);
+            // return only movie
+            res.json(movie);
+        } else { //Return movie and all reviews that go with movie
+            Review.find(function (err, reviews) {
+                if (err) res.send(err);
+                //find matching review movietitle
+                Review.find({ movietitle: movie.title }).exec(function (err, reviews) {
+                    if (err) res.send(err);
+                    res.json({ 
+                        movie:movie,
+                        reviews:reviews
+                    });
+                });
+            }); 
+        }
     });
 });
 
 //Get all
 router.get('/movies/getall', (req, res) => {
+    console.log("Going to get all movies...");
     Movie.find(function (err, movies) {
         if (err) res.send(err);
-        // return the users
+        var reviewOption = req.headers.review;
+        var userJson = JSON.stringify(movies);
+        // return only movies
         res.json(movies);
-      });
+    });
 });
 
 //Update    
 router.put('/movies/update', (req, res) => {
     console.log("Going to update a movie...");
     var id = req.headers._id;
-    Movie.findOne({_id: id}).exec(function(err, movie) {
-      if (err) res.send(err);
+    Movie.findOne({ _id: id }).exec(function (err, movie) {
+        if (err) res.send(err);
         // update the users info only if its new
         if (req.body.title) movie.title = req.body.title;
         if (req.body.yearReleased) movie.yearReleased = req.body.yearReleased;
         if (req.body.genre) movie.genre = req.body.genre;
         if (req.body.actors) movie.actors = req.body.actors;
 
-        // save the user
+        // save the movie
         movie.save(function (err) {
             if (err) res.send(err);
 
             // return a message
-            res.json({message: 'Movie updated!'});
-        });  
+            res.json({ message: 'Movie updated!' });
+        });
     });
 });
-
 
 //Delete
 router.delete('/movies/delete', (req, res) => {
     console.log("Going to delete a movie...");
     if (!req.body._id) {
-       res.json({success: false, msg: 'Please pass movie id.'});
-   } 
-   else { 
-        Movie.findOne({ _id: req.body._id }).select('_id').exec(function(err, movie) {
+        res.json({ success: false, msg: 'Please pass movie id.' });
+    }
+    else {
+        Movie.findOne({ _id: req.body._id }).select('_id').exec(function (err, movie) {
             if (err) res.send(err);
-        
-             movie.remove()
-            .then(item => {
-                res.json({success: true, msg: 'Successful removed movie.'});
-             })
-            .catch(err => {
-                res.status(422).send("Unable to remove movie from database");
-            });
+
+            movie.remove()
+                .then(item => {
+                    res.json({ success: true, msg: 'Successful removed movie.' });
+                })
+                .catch(err => {
+                    res.status(422).send("Unable to remove movie from database");
+                });
         });
     }
 });
@@ -274,26 +295,34 @@ router.delete('/movies/delete', (req, res) => {
 //Create
 router.post('/reviews/save', (req, res) => {
 
-    console.log("Going to save a movie...");
+    console.log("Going to save a review...");
 
     //Needs title, yearReleased, genre, >= 3 actorName, >= 3 actorCharacterName
-    if (    !req.body.reviewer
-         || !req.body.quote
-         || !req.body.rating 
-         || !req.body.movieid
-        ) {
-        res.json({success: false, msg: 'Please pass reviewer, quote, rating, and movie id.'});
-    } 
-    else { 
-        var newReview = new Review(req.body)
-        // save the user
-        newReview.save()
-            .then(item => {
-                res.json({success: true, msg: 'Successful created new review.'});
-            })
-            .catch(err => {
-                res.status(422).send("Unable to save review to database");
-            });
+    if (!req.body.reviewer
+        || !req.body.quote
+        || !req.body.rating
+        || !req.body.movietitle
+    ) {
+        res.json({ success: false, msg: 'Please pass reviewer, quote, rating, and movie title.' });
+    }
+    else {   
+        Movie.findOne({ title: req.body.movietitle }).select('title').exec(function (err, movie) {
+            if (err) res.send(err);
+            if (movie != null){
+                var newReview = new Review(req.body)
+                // save the user
+                newReview.save()
+                    .then(item => {
+                        res.json({ success: true, msg: 'Successful created new review.' });
+                    })
+                    .catch(err => {
+                        res.status(422).send("Unable to save review to database");
+                    });
+            } else {
+                res.status(422).send("Unable to save review to database, movie is not found.");
+            }
+
+        });
     }
 });
 
@@ -302,27 +331,50 @@ router.post('/reviews/save', (req, res) => {
 router.get('/reviews/get', (req, res) => {
     console.log("Going to get a review...");
     var id = req.headers._id;
-    Review.findById(id, function(err, review) {
-      if (err) res.send(err);
-  
-      var userJson = JSON.stringify(review);
-      // return that user
-      res.json(review);
+    Review.findById(id, function (err, review) {
+        if (err) res.send(err);
+
+        var userJson = JSON.stringify(review);
+        // return that review
+        res.json(review);
     });
 });
 
 //Get all
 router.get('/reviews/getall', (req, res) => {
+    console.log("Going to get all reviews...");
     Review.find(function (err, reviews) {
         if (err) res.send(err);
-        // return the users
+        // return the reviews
         res.json(reviews);
-      });
+    });
 });
+
+//Delete review
+router.delete('/reviews/delete', (req, res) => {
+    console.log("Going to delete a review...");
+    if (!req.body._id) {
+        res.json({ success: false, msg: 'Please pass review id.' });
+    }
+    else {
+        Review.findOne({ _id: req.body._id }).select('_id').exec(function (err, review) {
+            if (err) res.send(err);
+
+            review.remove()
+                .then(item => {
+                    res.json({ success: true, msg: 'Successful removed review.' });
+                })
+                .catch(err => {
+                    res.status(422).send("Unable to remove review from database");
+                });
+        });
+    }
+});
+
 //~~~~~~everything else~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// router.route('*', function(req, res, next) {
-//     res.status(405).send({message:"Unsupported method or invalid path."});
-//   });
+router.route('*', function(req, res, next) {
+    res.status(405).send({message:"Unsupported method or invalid path."});
+  });
 
 
 app.use('/', router);
